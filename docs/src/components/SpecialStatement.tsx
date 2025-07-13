4316c84dbc6d84052059fa1d22445779b385ce6b
Eyelly Wu <eyelly.wu@gmail.com>
2025-07-13 13:24:25 +0800

    docs: refactor special statement component
    - Simplify the structure of the SpecialStatement component
    - Consolidate multiple translations into a single string
    - Remove unnecessary line breaks and indentation

--- docs/src/components/SpecialStatement.tsx
+++ docs/src/components/SpecialStatement.tsx
@@ -11,16 +11,15 @@ export default function SpecialStatement(props: SpecialStatementProps) {
   return (
     <>
       <BlockQuote>
-        {t(
+        {`${t(
           '为了避免不必要的重复文档内容，该库的部分文档是链接{0}中的内容',
           getI18nPro('tag'),
         )}
-        <br />
-        {t(
-          '当前文档中{0}相关链接是基于{1}版本，如果你使用的非该版本，需查看你所使用版本对应的文档，以免用法不一致',
-          getI18nPro('tag'),
-          ` \`${i18nProPkg.version}\` `,
-        )}
+${t(
+  '当前文档中{0}相关链接是基于{1}版本，如果你使用的非该版本，需查看你所使用版本对应的文档，以免用法不一致',
+  getI18nPro('tag'),
+  ` \`${i18nProPkg.version}\` `,
+)}`}
       </BlockQuote>
     </>
   )
